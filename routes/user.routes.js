953d5bebd5a47f22ddad9237c4098b782c9a6c66
Shubham Baranwal <41940290+Sbwal@users.noreply.github.com>
2023-03-25 22:33:11 +0000

    user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,15 +5,30 @@ const { authMiddleware } = require('../middleware');
 const userRouter = express.Router();
 
 userRouter.get('/:id', authMiddleware, async (req, res) => {
-  // ...
-});
-
-userRouter.post('/:id/comment', authMiddleware, async (req, res) => {
-  // ...
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
 });
 
 userRouter.get('/:id/todos', authMiddleware, async (req, res) => {
-  // ...
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const todos = await Todo.find({ user: user._id });
+    res.json(todos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
 });
 
 module.exports = userRouter;
